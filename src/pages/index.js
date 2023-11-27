fbc0c289f4ac13d7148d49a149f3e491de9571b2
vishalEggoz <97423974+vishalEggoz@users.noreply.github.com>
2023-11-27 11:03:48 +0530

    added page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import Menu from '@/components/Menu'
 import Toolbox from '@/components/Toolbox'
 import Board from '@/components/Board'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>sketchbook</title>
+      </Head>
       <Menu />
       <Toolbox />
       <Board />
